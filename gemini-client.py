41047c03f79e02a5de687074e483a93e5a6bec65
Zachary McKee <zmckee@u.northwestern.edu>
2020-08-09 21:14:49 -0500

    allow user to select relative links and navigate to them

--- gemini-client.py
+++ gemini-client.py
@@ -1,8 +1,12 @@
 import socket
 import ssl
 import sys
+from urllib.parse import urlparse
+from typing import List, Set, Dict, Tuple, Optional
+import sys
+import time
 
-def get_header(conn, url):
+def get_header(conn, url) -> Tuple[str, str]:
   conn.write(url.encode())
   header = conn.recv(4096).decode()
 
@@ -13,7 +17,25 @@ def get_header(conn, url):
   return (status, meta)
   print(conn.recv().decode())
 
-def do_connection(url):
+def parse_links(body) -> Tuple[List[str], str]:
+  lines = body.split("\n")
+  link_count = 0
+  links = []
+  for i in range(len(lines)):
+    if lines[i][:2] == "=>":
+      link_count += 1
+      link_chunks = lines[i].replace("=>", f'[{link_count}]', 1)
+      link_chunks = link_chunks.split(maxsplit=2)
+      if len(link_chunks) == 3:
+        lines[i] = link_chunks[0] + " " + link_chunks[2]
+      else:
+        lines[i] = " ".join(link_chunks)
+      links.append(link_chunks[1])
+
+  body = "\n".join(lines)
+  return (links, body)
+
+def do_connection(url) -> List[str]:
   request = url + "\r\n"
 
   if url[:9] == "gemini://":
@@ -30,19 +52,49 @@ def do_connection(url):
     (header, meta) = get_header(conn, request)
     print("HEADER: " + header)
     print("META: " + meta)
+    body = ""
     if header[0] == '2':
       if meta == "text/gemini":
-        body = conn.recv().decode()
-        while body:
-          print(body)
-          body = conn.recv().decode()
+        new_block = conn.recv().decode()
+        while new_block:
+          body += new_block
+          new_block = conn.recv(4096).decode()
+        links, body = parse_links(body)
+        print(body)
+    elif header[0] == '3':
+      print("REDIRECTED TO " + meta)
+      time.sleep(1)
+      conn.close()
+      links = do_connection(meta)
   finally:
     conn.close()
+  return links
+
+def is_absolute(url):
+  return bool(urlparse(url).netloc)
+
 
 def client(url):
   print("Stellar Gemini Client")
-  do_connection(url)
+  current_url = url
+  while True:
+    links = do_connection(current_url)
+    print("List of links on this page:")
+    for i in range(len(links)):
+      print(f"{[i + 1]} -- ", end="")
+      print(links[i])
+    print(">>>", end=" ")
+    while True:
+      user_input = input()
+      user_num = int(user_input)
+      try:
+        selected_link = links[user_num-1]
+        break
+      except IndexError:
+        print("Bad link, please select a link in the list.")
+      print(">>>", end=" ")
 
+    current_url += selected_link
 if len(sys.argv) > 1:
   url = sys.argv[1]
 else:
