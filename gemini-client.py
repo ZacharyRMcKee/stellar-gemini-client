d20c2826272797ba8d392c003e0566fe8f851c87
Zachary McKee <zmckee@u.northwestern.edu>
2020-08-11 00:47:09 -0500

    use urllib to process some of the url processing

--- gemini-client.py
+++ gemini-client.py
@@ -2,20 +2,29 @@ import socket
 import ssl
 import sys
 from urllib.parse import urlparse
+from urllib.parse import urljoin
 from typing import List, Set, Dict, Tuple, Optional
 import sys
 import time
+import webbrowser
 
-def get_header(conn, url) -> Tuple[str, str]:
+from pathlib import Path
+
+
+
+def get_response(conn, url) -> Tuple[str, str, str]:
   conn.write(url.encode())
-  header = conn.recv(4096).decode()
-
-  status = header[:2]
-  space = header[2]
-  meta = header[3:-2]
-
-  return (status, meta)
-  print(conn.recv().decode())
+  response = ""
+  data = conn.recv().decode()
+  while data:
+    response += data
+    data = conn.recv(4096).decode()
+  response = response.split("\r\n",maxsplit=1)
+  header = response[0]
+  body = response[1]
+  response_code = header[:2]
+  meta = header[3:]
+  return response_code, meta, body
 
 def parse_links(body) -> Tuple[List[str], str]:
   lines = body.split("\n")
@@ -39,32 +48,29 @@ def do_connection(url) -> List[str]:
   request = url + "\r\n"
   print("CONNECTING TO " + url)
 
-  if url[:9] == "gemini://":
-    pruned_url = url[9:]
-  else:
-    pruned_url = url
-  hostname = pruned_url.split("/", 1)[0]
+  scheme, netloc, path, params, query, fragment = urlparse(url, scheme="gemini")
+
+  if scheme != "gemini":
+    webbrowser.open_new(url)
+    return []
   soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
   context.verify_mode = ssl.CERT_NONE
   context.check_hostname = False
   context.load_default_certs()
-  conn = context.wrap_socket(soc, server_hostname=hostname)
+  conn = context.wrap_socket(soc, server_hostname=netloc)
   try:
-    conn.connect((hostname, 1965))
-    (header, meta) = get_header(conn, request)
-    print("HEADER: " + header)
+    conn.connect((netloc, 1965))
+    response_code, meta, body = get_response(conn, request)
+    conn.close()
+    print("RESPONSE CODE: " + response_code)
     print("META: " + meta)
-    body = ""
-    if header[0] == '2':
+    if response_code[0] == '2':
       if meta == "text/gemini":
-        new_block = conn.recv().decode()
-        while new_block:
-          body += new_block
-          new_block = conn.recv(4096).decode()
+
         links, body = parse_links(body)
         print(body)
-    elif header[0] == '3':
+    elif response_code[0] == '3':
       print("REDIRECTED TO " + meta)
       time.sleep(1)
       conn.close()
@@ -83,18 +89,16 @@ def is_int(str):
   except ValueError:
     return False
 
-def get_new_link(input_url, link):
+def get_new_link(input_url, current_link):
   new_url = ""
-  if link[:9] == "gemini://":
-    link = link[9:]
+
+  new_url = urljoin(current_link, input_url)
+
   if is_absolute(input_url):
     new_url = input_url
-  elif input_url == "..":
-    new_url = link.rsplit('/', 1)[0]
   else:
-    new_url = link + input_url
-  if new_url[:9] != "gemini://":
-    new_url = "gemini://" + new_url
+    current_link_parsed = urlparse(current_link)
+
   return new_url
 
 
