129239c43967348a3986c84bfdbfd85244049c4e
Zachary McKee <zmckee@u.northwestern.edu>
2020-08-16 13:38:02 -0500

    Add support for encodings other than utf-8.

--- gemini-client.py
+++ gemini-client.py
@@ -1,7 +1,7 @@
 import socket
 import ssl
 import sys
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlunparse
 from urllib.parse import urljoin
 from typing import List, Set, Dict, Tuple, Optional
 import sys
@@ -11,20 +11,53 @@ import webbrowser
 from pathlib import Path
 
 
+def parse_mime(meta, response_code) -> Tuple[str, List[Tuple[str, str]]]:
+  assert response_code[0] == "2"
 
-def get_response(conn, url) -> Tuple[str, str, str]:
+  tokenized_meta = [token.strip() for token in meta.split(";")]
+  tokenized_meta = [i for i in tokenized_meta if i]
+  body_type, parameters = (tokenized_meta[0], tokenized_meta[1:])
+  tokenized_params = [tuple(parameter.split("=")) for parameter in parameters]
+  return body_type, tokenized_params
+
+
+
+def decode_body(body, params) -> str:
+  charset = "utf-8"
+  for (attribute, value) in params:
+    if attribute == "charset":
+      charset = value.lower()
+  body = body.decode(encoding=charset)
+  return body
+
+
+def get_response(conn, url) -> Tuple[str, str, Tuple[str, str]]:
   conn.write(url.encode())
-  response = ""
-  data = conn.recv().decode()
+  response = []
+  data = conn.recv()
   while data:
-    response += data
-    data = conn.recv(4096).decode()
-  response = response.split("\r\n",maxsplit=1)
-  header = response[0]
-  body = response[1]
+    response.append(data)
+    data = conn.recv(4096)
+
+  response = b"".join(response)
+  response = response.split(b"\r\n",maxsplit=1)
+  header = response[0].decode()
   response_code = header[:2]
   meta = header[3:]
-  return response_code, meta, body
+  if response_code[0] == "2":
+    body_type, params = parse_mime(meta, response_code)
+    body = decode_body(response[1], params)
+  else:
+    body_type = ""
+    body = ("","")
+  return response_code, meta, (body_type, body)
+
+
+
+
+
+
+
 
 def parse_links(body) -> Tuple[List[str], str]:
   lines = body.split("\n")
@@ -52,7 +85,7 @@ def do_connection(url) -> List[str]:
 
   if scheme != "gemini":
     webbrowser.open_new(url)
-    return []
+    return ([], "")
   soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
   context.verify_mode = ssl.CERT_NONE
@@ -61,57 +94,66 @@ def do_connection(url) -> List[str]:
   conn = context.wrap_socket(soc, server_hostname=netloc)
   try:
     conn.connect((netloc, 1965))
-    response_code, meta, body = get_response(conn, request)
+    response_code, meta, (body_type, body) = get_response(conn, request)
     conn.close()
     print("RESPONSE CODE: " + response_code)
     print("META: " + meta)
     if response_code[0] == '2':
-      if meta == "text/gemini":
-
-        links, body = parse_links(body)
-        print(body)
+      links, body = parse_links(body)
+      print(body)
     elif response_code[0] == '3':
       print("REDIRECTED TO " + meta)
       time.sleep(1)
       conn.close()
-      links = do_connection(meta)
+      url = meta
+      links, url = do_connection(meta)
   finally:
     conn.close()
-  return links
+  return links, url
 
-def is_absolute(url):
+def is_absolute(url) -> bool:
   return bool(urlparse(url).netloc)
 
-def is_int(str):
+def is_int(str) -> bool:
   try:
     int(str)
     return True
   except ValueError:
     return False
 
-def get_new_link(input_url, current_link):
+def get_new_link(input_url, current_link) -> str:
   new_url = ""
+  current_link_parsed = urlparse(current_link)
+  input_url_parsed = urlparse(input_url)
+  if current_link_parsed.scheme == "gemini":
+    current_link_parsed = current_link_parsed._replace(scheme="")
+  if input_url_parsed.scheme == "gemini":
+    input_url_parsed =  input_url_parsed._replace(scheme="")
+  current_link = urlunparse(current_link_parsed)
+  input_url = urlunparse(input_url_parsed)
 
   new_url = urljoin(current_link, input_url)
 
-  if is_absolute(input_url):
-    new_url = input_url
-  else:
-    current_link_parsed = urlparse(current_link)
+  if urlparse(new_url).scheme == "":
+    new_url = urlparse(new_url, scheme="gemini")
+    new_url = urlunparse(new_url)
 
   return new_url
 
 
-def client(url):
+def print_repl_prompt() -> None:
+  print(">>>", end=" ")
+
+def client(url) -> None:
   print("Stellar Gemini Client")
   current_url = url
   while True:
-    links = do_connection(current_url)
+    links, current_url = do_connection(current_url)
     print("List of links on this page:")
     for i in range(len(links)):
       print(f"{[i + 1]} -- ", end="")
       print(links[i])
-    print(">>>", end=" ")
+    print_repl_prompt()
     while True:
       user_input = input()
       if is_int(user_input):
@@ -125,7 +167,7 @@ def client(url):
         selected_link = user_input
         break
 
-      print(">>>", end=" ")
+      print_repl_prompt()
     current_url = get_new_link(selected_link, current_url)
 if len(sys.argv) > 1:
   url = sys.argv[1]
